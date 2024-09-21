ee35cd187e6d6fd663932095d620e7cfc3da368d
sebastian sovier <sebastian.sovier@tgtgroup.cl>
2024-09-21 16:38:29 -0300

    cambios

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+const app = createApp(App)
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
 // Make BootstrapVue available throughout your project
 // Optionally install the BootstrapVue icon components plugin
-
-createApp(App).mount('#app')
+app.use(bootstrap)
+app.use(router)
+app.mount('#app')
